e588a1407c060237c58e189faa3e030a92bc5d34
Anton K <akarpenko83@gmail.com>
2023-04-06 12:54:43 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
-import './css/styles.css';
-import { refs } from './js/refs';
-import { fetchCountries } from './js/fetchCountries'
+const refs = {
+    formRef: document.querySelector('form'),
+}
 
-const debounce = require('lodash.debounce');
-const DEBOUNCE_DELAY = 300;
 
-refs.inputRef.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY))
+refs.formRef.addEventListener('submit', onSearch);
+
+// ======================== pixabay ==================================
+let searchQuery = '';
+const API_KEY = '35064628-b4315bc92921e9ccef2ae28e5';
+const IMAGE_TYPE = 'image_type=photo';
+const ORIENTATION = 'orientation=horizontal';
+const SAFESEARCH = 'safesearch=true';
+
+
+function onSearch(event) {
+    event.preventDefault();
+    searchQuery = event.currentTarget.elements.searchQuery.value;
+    let url = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeQuery(searchQuery)}&${IMAGE_TYPE}&${ORIENTATION}&${SAFESEARCH}`;
+    console.log(url);
+    fetch(url)
+        .then(data => {
+        console.log(data);
+        return data.json()
+        
+    })
+        .then(data => console.dir(data))
+        .catch(err => console.error(err));
+
+ }
+
+function encodeQuery(searchQuery) {
+    return encodeURIComponent(searchQuery).replace(/%20/g, "+");
+ }
+
+
+
+
+
 
 
 
--- src/js/refs.js
+++
@@ -1,5 +0,0 @@
-export const refs = {
-    inputRef: document.querySelector('input'),
-    ulRef: document.querySelector('.country-list'),
-    infoRef: document.querySelector('.country-info'),
-}
